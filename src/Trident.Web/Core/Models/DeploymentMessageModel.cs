92d35778a2aee1ef6b1eb0fa18af517384526acd
bobjwalker <Bob.Walker@octopus.com>
2025-05-21 16:45:19 -0500

    TRID-33: Creating the message models for the pub sub to share with spear

---
+++ src/Trident.Web/Core/Models/DeploymentMessageModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Identity.Client;
+using System;
+
+namespace Trident.Web.Core.Models
+{
+    public class DeploymentMessageModel
+    {
+        public string InstanceId { get; set; }
+        public string DeploymentId { get; set; }
+        
+        public string SpaceId { get; set; }
+        public string SpaceName { get; set; }
+
+        public string EnvironmentId { get; set; }
+        public string EnvironmentName { get; set; }
+
+        public string ProjectId { get; set; }
+        public string ProjectName { get; set; }        
+
+        public string TenantId { get; set; }
+        public string TenantName { get; set; }
+
+        public string ReleaseId { get; set; }
+        public string ReleaseNumber { get; set; }
+        public DateTime ReleaseCreationDate { get; set; }
+
+        public DateTime QueueTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime CompletedTime { get; set; }
+        public string DeploymentState { get; set; }
+    }
+}
---
+++ src/Trident.Web/Core/Models/DeploymentRequestMessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Trident.Web.Core.Models
+{
+    public class DeploymentRequestMessageModel
+    {
+        public string InstanceId { get; set; }
+        public string ApiKey { get; set; }
+        public string DeploymentRequestId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime RequestQueueTime { get; set; }
+    }
+}
